5ced46df480acf2d322e3bb2fe0c182b67e5eb93
NIC619 <twedusuck@gmail.com>
2020-07-09 22:34:49 +0800

    Upload Buidler/ts configs

---
+++ buidler.config.ts
@@ -0,0 +1,22 @@
+import { BuidlerConfig, usePlugin, task } from "@nomiclabs/buidler/config";
+
+usePlugin("@nomiclabs/buidler-waffle");
+
+// This is a sample Buidler task. To learn how to create your own go to
+// https://buidler.dev/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, bre) => {
+  const accounts = await bre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(await account.getAddress());
+  }
+});
+
+const config: BuidlerConfig = {
+  defaultNetwork: "buidlerevm",
+  solc: {
+    version: "0.5.16",
+  },
+};
+
+export default config;
