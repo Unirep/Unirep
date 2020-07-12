e89de156d6b14a82c40661d9b3d0959106ff5e87
NIC619 <twedusuck@gmail.com>
2020-07-12 22:34:48 +0800

    Add config

---
+++ config/testLocal.ts
@@ -0,0 +1,11 @@
+const globalStateTreeDepth = 4;
+
+const userStateTreeDepth = 4;
+
+const maxUsers = 2 ** globalStateTreeDepth - 1;
+
+export {
+    globalStateTreeDepth,
+    userStateTreeDepth,
+    maxUsers
+}
