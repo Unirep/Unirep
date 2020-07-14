5d3a7edc369965bc677aa2eeda941d0be2af2353
NIC619 <twedusuck@gmail.com>
2020-07-14 17:43:22 +0800

    Add attesterSignUpViaRelayer test cases

--- test/SignUp.ts
+++ test/SignUp.ts
@@ -14,6 +14,7 @@ const { expect } = chai
 import Unirep from "../artifacts/Unirep.json"
 import PoseidonT3 from "../artifacts/PoseidonT3.json"
 import PoseidonT6 from "../artifacts/PoseidonT6.json"
+import { splitSignature } from "ethers/lib/utils"
 
 
 describe('IncrementalMerkleTree', () => {
@@ -124,25 +125,59 @@ describe('IncrementalMerkleTree', () => {
 
     describe('Attester sign-ups', () => {
         let attester
+        let attesterAddress
+        let attester2
+        let attester2Address
+        let attester2Sig
         let unirepContractCalledByAttester
 
         it('sign up should succeed', async () => {
             attester = accounts[1]
+            attesterAddress = await attester.getAddress()
             unirepContractCalledByAttester = await ethers.getContractAt(Unirep.abi, unirepContract.address, attester)
             const tx = await unirepContractCalledByAttester.attesterSignUp()
             const receipt = await tx.wait()
 
             expect(receipt.status).equal(1)
 
-            const attesterId = await unirepContract.attesters(await attester.getAddress())
+            const attesterId = await unirepContract.attesters(attesterAddress)
             expect((1).toString()).equal(attesterId.toString())
             const nextAttesterId_ = await unirepContract.nextAttesterId()
             // nextAttesterId starts with 1 so now it should be 2
             expect((2).toString()).equal(nextAttesterId_.toString())
         })
 
+        it('sign up via relayer should succeed', async () => {
+            let relayer = accounts[0]
+            attester2 = accounts[2]
+            attester2Address = await attester2.getAddress()
+
+            let message = ethers.utils.solidityKeccak256(["address", "address"], [attester2Address, unirepContract.address])
+            let sigHex = await attester2.signMessage(ethers.utils.arrayify(message))
+            attester2Sig = splitSignature(sigHex)
+            const tx = await unirepContract.attesterSignUpViaRelayer(attester2Address, attester2Sig.v, attester2Sig.r, attester2Sig.s)
+            const receipt = await tx.wait()
+            
+            expect(receipt.status).equal(1)
+
+            const attesterId = await unirepContract.attesters(attester2Address)
+            expect((2).toString()).equal(attesterId.toString())
+            const nextAttesterId_ = await unirepContract.nextAttesterId()
+            expect((3).toString()).equal(nextAttesterId_.toString())
+        })
+
+        it('sign up with invalid signature should fail', async () => {
+            let attester3 = accounts[3]
+            let attester3Address = await attester3.getAddress()
+            await expect(unirepContract.attesterSignUpViaRelayer(attester3Address, attester2Sig.v, attester2Sig.r, attester2Sig.s))
+                .to.be.revertedWith('Unirep: invalid attester sign up signature')
+        })
+
         it('double sign up should fail', async () => {
             await expect(unirepContractCalledByAttester.attesterSignUp())
+                .to.be.revertedWith('Unirep: attester has already signed up')
+
+            await expect(unirepContract.attesterSignUpViaRelayer(attester2Address, attester2Sig.v, attester2Sig.r, attester2Sig.s))
                 .to.be.revertedWith('Unirep: attester has already signed up')
         })
     })
