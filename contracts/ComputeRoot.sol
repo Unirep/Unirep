2d5ee8b191573e40cb28ec395d5286f87c7d65cc
NIC619 <twedusuck@gmail.com>
2020-08-03 17:31:48 +0800

    Increase `computeEmptyRoot` tree level limit

--- contracts/ComputeRoot.sol
+++ contracts/ComputeRoot.sol
@@ -10,8 +10,8 @@ contract ComputeRoot is Hasher {
     function computeEmptyRoot(uint8 _treeLevels, uint256 _zeroValue) public pure returns (uint256) {
         // Limit the Merkle tree to MAX_DEPTH levels
         require(
-            _treeLevels > 0 && _treeLevels <= 32,
-            "ComputeRoot: _treeLevels must be between 0 and 33"
+            _treeLevels > 0 && _treeLevels <= 256,
+            "ComputeRoot: _treeLevels must be between 0 and 256"
         );
 
         uint256 currentZero = _zeroValue;
