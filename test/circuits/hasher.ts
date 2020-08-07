3ef4106f7da84cc3cafb2697062519533c05a15e
NIC619 <twedusuck@gmail.com>
2020-08-07 21:52:27 +0800

    Add hasher circuit test (copied from MACI)

---
+++ test/circuits/hasher.ts
@@ -0,0 +1,35 @@
+import chai from "chai"
+
+const { expect } = chai
+
+import {
+    compileAndLoadCircuit,
+} from './utils'
+
+import {
+    stringifyBigInts,
+    genRandomSalt,
+    hashLeftRight,
+} from 'maci-crypto'
+
+describe('Poseidon hash circuits', () => {
+
+    it('correctly hashes two random values', async () => {
+        const circuit = await compileAndLoadCircuit('test/hashleftright_test.circom')
+
+        const left = genRandomSalt()
+        const right = genRandomSalt()
+
+        const circuitInputs = stringifyBigInts({ left, right })
+
+        const witness = circuit.calculateWitness(circuitInputs)
+        expect(circuit.checkWitness(witness)).to.be.true
+
+        const outputIdx = circuit.getSignalIdx('main.hash')
+        const output = witness[outputIdx]
+
+        const outputJS = hashLeftRight(left, right)
+
+        expect(output.toString()).equal(outputJS.toString())
+    })
+})
