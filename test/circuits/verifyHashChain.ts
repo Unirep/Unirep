62fe82c006b7cfcf91a6fa0ab5ed2f18db1c0ffa
NIC619 <twedusuck@gmail.com>
2020-08-18 21:08:13 +0800

    Update hash chain verifier test

--- test/circuits/verifyHashChain.ts
+++ test/circuits/verifyHashChain.ts
@@ -17,7 +17,7 @@ describe('Hash chain circuit', () => {
 
     const NUM_ELEMENT = 10
     let elements: SnarkBigInt[] = []
-    let cur = 0, result
+    let cur = 0, result, selectors: number[] = []
 
     let resultNotMatchRegExp: RegExp
 
@@ -26,8 +26,12 @@ describe('Hash chain circuit', () => {
 
         for (let i = 0; i < NUM_ELEMENT; i++) {
             const element = genRandomSalt()
+            const sel = Math.floor(Math.random() * 2)
+            selectors.push(sel)
             elements.push(element)
-            cur = hashLeftRight(element, cur)
+            if ( sel == 1) {
+                cur = hashLeftRight(element, cur)
+            }
         }
         result = cur
     })
@@ -36,6 +40,7 @@ describe('Hash chain circuit', () => {
         const circuitInputs = {
             in_first: 0,
             in_rest: elements,
+            selectors: selectors,
             result: result
         }
 
@@ -48,6 +53,7 @@ describe('Hash chain circuit', () => {
         const circuitInputs = {
             in_first: incorrectFirstIn,
             in_rest: elements,
+            selectors: selectors,
             result: result
         }
 
@@ -62,6 +68,7 @@ describe('Hash chain circuit', () => {
         const circuitInputs = {
             in_first: 0,
             in_rest: elements,
+            selectors: selectors,
             result: result
         }
 
@@ -71,11 +78,28 @@ describe('Hash chain circuit', () => {
         elements.reverse()
     })
 
+    it('verify with incorrect selectors should fail', async () => {
+        const wrongSelectors = selectors.slice()
+        // Flip one of the selector
+        wrongSelectors[NUM_ELEMENT - 5] = wrongSelectors[NUM_ELEMENT - 5] ? 0 : 1
+        const circuitInputs = {
+            in_first: 0,
+            in_rest: elements,
+            selectors: wrongSelectors,
+            result: result
+        }
+
+        expect(() => {
+            circuit.calculateWitness(circuitInputs)
+        }).to.throw(resultNotMatchRegExp)
+    })
+
     it('verify incorrect number of elements should fail', async () => {
         const signalNotAssignedRegExp = RegExp('^Input Signal not assigned:.+')
         const circuitInputs = {
             in_first: elements[0],
             in_rest: elements.slice(1),
+            selectors: selectors,
             result: result
         }
 
@@ -89,6 +113,7 @@ describe('Hash chain circuit', () => {
         const circuitInputs = {
             in_first: 0,
             in_rest: elements,
+            selectors: selectors,
             result: incorrectResult
         }
 
