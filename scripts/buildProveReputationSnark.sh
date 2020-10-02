69cfad2177536fba92230f54200c5c717da6236b
NIC619 <twedusuck@gmail.com>
2020-10-03 00:37:40 +0800

    Update zk build scripts

--- scripts/buildProveReputationSnark.sh
+++ scripts/buildProveReputationSnark.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")"
 cd ..
 mkdir -p build
 
-NODE_OPTIONS=--max-old-space-size=8192 npx ts-node scripts/buildSnarks.ts -i circuits/test/proveReputation_test.circom -j build/proveReputationCircuit.r1cs -w build/proveReputation.wasm -p build/proveReputationPk.json -v build/proveReputationVk.json -s build/ReputationVerifier.sol -vs ReputationVerifier -z build/proveReputation.zkey
+NODE_OPTIONS=--max-old-space-size=8192 npx ts-node scripts/buildSnarks.ts -i circuits/test/proveReputation_test.circom -j build/proveReputationCircuit.r1cs -w build/proveReputation.wasm -p build/proveReputationPk.json -v build/proveReputationVk.json -s build/ReputationVerifier.sol -vs ReputationVerifier -pr build/proveReputation.params
 
 echo 'Copying ReputationVerifier.sol to contracts/'
 cp ./build/ReputationVerifier.sol ./contracts/
