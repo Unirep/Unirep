52eca776dc0310555e40ec81bc62c8373c4679cd
NIC619 <twedusuck@gmail.com>
2020-10-17 00:10:12 +0800

    Add genUserStateFromContract

--- core/utils.ts
+++ core/utils.ts
@@ -10,6 +10,8 @@ import Unirep from "../artifacts/Unirep.json"
 import { numAttestationsPerBatch } from '../config/testLocal'
 import { Attestation, IEpochTreeLeaf, UnirepState } from './UnirepState'
 import { IUserStateLeaf, UserState } from './UserState'
+import { hashLeftRight } from 'maci-crypto'
+import { computeEmptyUserStateRoot } from '../test/utils'
 
 /*
  * Retrieves and parses on-chain Unirep contract data to create an off-chain
@@ -140,7 +142,11 @@ const genUnirepStateFromContract = async (
                 userStateTransitionedEvent.args?._fromNullifierTreeRoot,
                 userStateTransitionedEvent.args?._proof,
             )
-            expect(isProofValid).to.be.true
+            // Proof is invalid, skip this step
+            if (!isProofValid) {
+                console.log("Invalid UserStateTransitioned proof")
+                continue
+            }
 
             const allNullifiers = userStateTransitionedEvent.args?._nullifiers.map((n) => BigInt(n))
             const noAtteNullifier = BigInt(userStateTransitionedEvent.args?._noAttestationNullifier)
@@ -162,8 +168,8 @@ const genUnirepStateFromContract = async (
  * @param provider An Ethereum provider
  * @param address The address of the MACI contract
  * @param startBlock The block number when Unirep contract is deployed
- * @param id The semaphore identity of the user
- * @param commitment The identity commitment
+ * @param userIdentity The semaphore identity of the user
+ * @param userIdentityCommitment The identity userIdentityCommitment
  * @param latestTransitionedEpoch Latest epoch user has transitioned to
  * @param latestGSTLeafIndex Leaf index in the global state tree of the latest epoch user has transitioned to
  * @param latestUserStateLeaves User state leaves (empty if no attestations received)
@@ -173,8 +179,8 @@ const genUserStateFromParams = async (
     provider: ethers.providers.Provider,
     address: string,
     startBlock: number,
-    id: any,
-    commitment: any,
+    userIdentity: any,
+    userIdentityCommitment: any,
     latestTransitionedEpoch: number,
     latestGSTLeafIndex: number,
     latestUserStateLeaves?: IUserStateLeaf[],
@@ -187,8 +193,8 @@ const genUserStateFromParams = async (
     )
     const userState = new UserState(
         unirepState,
-        id,
-        commitment,
+        userIdentity,
+        userIdentityCommitment,
         true,
         latestTransitionedEpoch,
         latestGSTLeafIndex,
@@ -198,7 +204,228 @@ const genUserStateFromParams = async (
     return userState
 }
 
+/*
+ * This function works mostly the same as genUnirepStateFromContract,
+ * except that it also updates the user's state during processing events.
+ * @param provider An Ethereum provider
+ * @param address The address of the MACI contract
+ * @param startBlock The block number when Unirep contract is deployed
+ */
+const _genUserStateFromContract = async (
+    provider: ethers.providers.Provider,
+    address: string,
+    startBlock: number,
+    userIdentity: any,
+    userIdentityCommitment: any,
+) => {
+
+    const unirepContract = new ethers.Contract(
+        address,
+        Unirep.abi,
+        provider,
+    )
+
+    const treeDepths_ = await unirepContract.treeDepths()
+    const globalStateTreeDepth = treeDepths_.globalStateTreeDepth
+    const userStateTreeDepth = treeDepths_.userStateTreeDepth
+    const epochTreeDepth = treeDepths_.epochTreeDepth
+    const nullifierTreeDepth = treeDepths_.nullifierTreeDepth
+    const attestingFee = await unirepContract.attestingFee()
+    const epochLength = (await unirepContract.epochLength()).toNumber()
+    const maxEpochKeyNonce = await unirepContract.maxEpochKeyNonce()
+
+    const unirepState = new UnirepState(
+        globalStateTreeDepth,
+        userStateTreeDepth,
+        epochTreeDepth,
+        nullifierTreeDepth,
+        attestingFee,
+        epochLength,
+        maxEpochKeyNonce,
+        numAttestationsPerBatch,
+    )
+
+    const userState = new UserState(
+        unirepState,
+        userIdentity,
+        userIdentityCommitment,
+        false,
+    )
+    const emptyUserStateRoot = computeEmptyUserStateRoot(unirepState.userStateTreeDepth)
+    const userDefaultGSTLeaf = hashLeftRight(
+        userIdentityCommitment,
+        emptyUserStateRoot
+    )
+
+    const newGSTLeafInsertedFilter = unirepContract.filters.NewGSTLeafInserted()
+    const newGSTLeafInsertedEvents =  await unirepContract.queryFilter(newGSTLeafInsertedFilter, startBlock)
+
+    const attestationSubmittedFilter = unirepContract.filters.AttestationSubmitted()
+    const attestationSubmittedEvents =  await unirepContract.queryFilter(attestationSubmittedFilter, startBlock)
+
+    const epochEndedFilter = unirepContract.filters.EpochEnded()
+    const epochEndedEvents =  await unirepContract.queryFilter(epochEndedFilter, startBlock)
+
+    const userStateTransitionedFilter = unirepContract.filters.UserStateTransitioned()
+    const userStateTransitionedEvents =  await unirepContract.queryFilter(userStateTransitionedFilter, startBlock)
+
+    const sequencerFilter = unirepContract.filters.Sequencer()
+    const sequencerEvents =  await unirepContract.queryFilter(sequencerFilter, startBlock)
+
+    // Reverse the events so pop() can start from the first event
+    newGSTLeafInsertedEvents.reverse()
+    attestationSubmittedEvents.reverse()
+    epochEndedEvents.reverse()
+    userStateTransitionedEvents.reverse()
+    let userHasSignedUp = false
+    let currentEpochGSTLeafIndexToInsert = 0
+    const epochKeyNonce = 0
+    let latestUserState
+    for (let i = 0; i < sequencerEvents.length; i++) {
+        const sequencerEvent = sequencerEvents[i]
+        const occurredEvent = sequencerEvent.args?._event
+        if (occurredEvent === "UserSignUp") {
+            const newLeafEvent = newGSTLeafInsertedEvents.pop()
+            assert(newLeafEvent !== undefined, `Event sequence mismatch: missing newGSTLeafInsertedEvent`)
+
+            const newLeaf = BigInt(newLeafEvent.args?._hashedLeaf)
+            expect(newLeaf).equal(newLeaf)
+            unirepState.signUp(unirepState.currentEpoch, newLeaf)
+            // New leaf matches default leaf means user signed up.
+            if (userDefaultGSTLeaf === newLeaf) {
+                userState.signUp(unirepState.currentEpoch, currentEpochGSTLeafIndexToInsert)
+                userHasSignedUp = true
+            }
+
+            // A user sign up, increment (next) GST leaf index
+            currentEpochGSTLeafIndexToInsert ++
+        } else if (occurredEvent === "AttestationSubmitted") {
+            const attestationEvent = attestationSubmittedEvents.pop()
+            assert(attestationEvent !== undefined, `Event sequence mismatch: missing attestationSubmittedEvent`)
+            const epoch = attestationEvent.args?._epoch
+            assert(
+                epoch.toNumber() === unirepState.currentEpoch,
+                `Attestation epoch (${epoch.toNumber()}) does not match current epoch (${unirepState.currentEpoch})`
+            )
+
+            const _attestation = attestationEvent.args?.attestation
+            const attestation = new Attestation(
+                BigInt(_attestation.attesterId),
+                _attestation.posRep.toNumber(),
+                _attestation.negRep.toNumber(),
+                BigInt(_attestation.graffiti),
+                _attestation.overwriteGraffiti
+            )
+            unirepState.addAttestation(attestationEvent.args?._epochKey.toString(), attestation)
+        } else if (occurredEvent === "EpochEnded") {
+            const epochEndedEvent = epochEndedEvents.pop()
+            assert(epochEndedEvent !== undefined, `Event sequence mismatch: missing epochEndedEvent`)
+            const epoch = epochEndedEvent.args?._epoch
+            assert(
+                epoch.toNumber() === unirepState.currentEpoch,
+                `Ended epoch (${epoch.toNumber()}) does not match current epoch (${unirepState.currentEpoch})`
+            )
+
+            // Get epoch tree leaves of the ending epoch
+            let [epochKeys_, epochKeyHashchains_] = await unirepContract.getEpochTreeLeaves(epoch)
+            epochKeys_ = epochKeys_.map((epk) => BigInt(epk.toString()))
+            epochKeyHashchains_ = epochKeyHashchains_.map((hc) => BigInt(hc.toString()))
+            const epochTreeLeaves: IEpochTreeLeaf[] = []
+            for (let i = 0; i < epochKeys_.length; i++) {
+                const epochTreeLeaf: IEpochTreeLeaf = {
+                    epochKey: epochKeys_[i],
+                    hashchainResult: epochKeyHashchains_[i]
+                }
+                epochTreeLeaves.push(epochTreeLeaf)
+            }
+
+            unirepState.epochTransition(epoch.toNumber(), epochTreeLeaves)
+            if (userHasSignedUp) {
+                if (epoch.toNumber() === userState.latestTransitionedEpoch) {
+                    // Latest epoch user transitioned to ends, keep a record of user state
+                    // at this moment in order to identify `UserStateTransitioned` event
+                    latestUserState = await userState.genNewUserStateAfterTransition(epochKeyNonce)
+                }
+            }
+
+            // Epoch ends, reset (next) GST leaf index
+            currentEpochGSTLeafIndexToInsert = 0
+        } else if (occurredEvent === "UserStateTransitioned") {
+            const newLeafEvent = newGSTLeafInsertedEvents.pop()
+            assert(newLeafEvent !== undefined, `Event sequence mismatch: missing newGSTLeafInsertedEvent`)
+            const userStateTransitionedEvent = userStateTransitionedEvents.pop()
+            assert(userStateTransitionedEvent !== undefined, `Event sequence mismatch: missing userStateTransitionedEvent`)
+
+            const newLeaf = newLeafEvent.args?._hashedLeaf
+            expect(newLeaf).equal(BigInt(newLeaf))
+
+            const isProofValid = await unirepContract.verifyUserStateTransition(
+                newLeaf,
+                userStateTransitionedEvent.args?._nullifiers,
+                userStateTransitionedEvent.args?._noAttestationNullifier,
+                userStateTransitionedEvent.args?._fromEpoch,
+                userStateTransitionedEvent.args?._fromGlobalStateTree,
+                userStateTransitionedEvent.args?._fromEpochTree,
+                userStateTransitionedEvent.args?._fromNullifierTreeRoot,
+                userStateTransitionedEvent.args?._proof,
+            )
+            // Proof is invalid, skip this step
+            if (!isProofValid) {
+                console.log("Invalid UserStateTransitioned proof")
+                continue
+            }
+
+            const allNullifiers = userStateTransitionedEvent.args?._nullifiers.map((n) => BigInt(n))
+            const noAtteNullifier = BigInt(userStateTransitionedEvent.args?._noAttestationNullifier)
+            allNullifiers.push(noAtteNullifier)
+
+            unirepState.userStateTransition(unirepState.currentEpoch, BigInt(newLeaf), allNullifiers)
+            if (userHasSignedUp && userStateTransitionedEvent.args?._fromEpoch.toNumber() === userState.latestTransitionedEpoch) {
+                assert(latestUserState !== undefined, "latestUserState can not be undefined since user's latest transitioned epoch has ended")
+                if (latestUserState.newGSTLeaf === BigInt(newLeaf)) {
+                    userState.transition(unirepState.currentEpoch, currentEpochGSTLeafIndexToInsert, latestUserState.newUSTLeaves)
+                }
+                latestUserState = undefined  // Set to undefined to prevent latest state from being mistakenly reused
+            }
+
+            // A user transition to this epoch, increment (next) GST leaf index
+            currentEpochGSTLeafIndexToInsert ++
+        } else {
+            throw  new Error(`Unexpected event: ${occurredEvent}`)
+        }
+    }
+    assert(userHasSignedUp, "User did not sign up")
+    return userState
+}
+
+/*
+ * Given user identiy and userIdentityCommitment, retrieves and parses on-chain
+ * Unirep contract data to create an off-chain representation as a
+ * UserState object (including UnirepState object).
+ * @param provider An Ethereum provider
+ * @param address The address of the MACI contract
+ * @param startBlock The block number when Unirep contract is deployed
+ * @param userIdentity The semaphore identity of the user
+ * @param userIdentityCommitment The identity userIdentityCommitment
+ */
+const genUserStateFromContract = async (
+    provider: ethers.providers.Provider,
+    address: string,
+    startBlock: number,
+    userIdentity: any,
+    userIdentityCommitment: any,
+) => {
+    return await _genUserStateFromContract(
+        provider,
+        address,
+        startBlock,
+        userIdentity,
+        userIdentityCommitment,
+    )
+}
+
 export {
     genUnirepStateFromContract,
+    genUserStateFromContract,
     genUserStateFromParams,
 }
