177cb024eb9a53e284c393e5fccfadfee6a65f82
NIC619 <twedusuck@gmail.com>
2020-10-21 23:37:33 +0800

    Add cli/epochTransition

---
+++ cli/epochTransition.ts
@@ -0,0 +1,145 @@
+import { Contract, Wallet, providers } from 'ethers'
+
+import {
+    promptPwd,
+    validateEthSk,
+    validateEthAddress,
+    checkDeployerProviderConnection,
+    contractExists,
+} from './utils'
+
+import { DEFAULT_ETH_PROVIDER } from './defaults'
+
+import Unirep from "../artifacts/Unirep.json"
+
+const configureSubparser = (subparsers: any) => {
+    const parser = subparsers.addParser(
+        'epochTransition',
+        { addHelp: true },
+    )
+
+    parser.addArgument(
+        ['-e', '--eth-provider'],
+        {
+            action: 'store',
+            type: 'string',
+            help: `A connection string to an Ethereum provider. Default: ${DEFAULT_ETH_PROVIDER}`,
+        }
+    )
+
+    parser.addArgument(
+        ['-t', '--is-test'],
+        {
+            action: 'storeTrue',
+            help: 'Indicate if the provider is a testing environment',
+        }
+    )
+
+    parser.addArgument(
+        ['-x', '--contract'],
+        {
+            required: true,
+            type: 'string',
+            help: 'The Unirep contract address',
+        }
+    )
+
+    const privkeyGroup = parser.addMutuallyExclusiveGroup({ required: true })
+
+    privkeyGroup.addArgument(
+        ['-dp', '--prompt-for-eth-privkey'],
+        {
+            action: 'storeTrue',
+            help: 'Whether to prompt for the user\'s Ethereum private key and ignore -d / --eth-privkey',
+        }
+    )
+
+    privkeyGroup.addArgument(
+        ['-d', '--eth-privkey'],
+        {
+            action: 'store',
+            type: 'string',
+            help: 'The deployer\'s Ethereum private key',
+        }
+    )
+}
+
+const epochTransition = async (args: any) => {
+
+    // Unirep contract
+    if (!validateEthAddress(args.contract)) {
+        console.error('Error: invalid Unirep contract address')
+        return
+    }
+
+    const unirepAddress = args.contract
+
+    // Ethereum provider
+    const ethProvider = args.eth_provider ? args.eth_provider : DEFAULT_ETH_PROVIDER
+
+    let ethSk
+    // The deployer's Ethereum private key
+    // The user may either enter it as a command-line option or via the
+    // standard input
+    if (args.prompt_for_eth_privkey) {
+        ethSk = await promptPwd('Your Ethereum private key')
+    } else {
+        ethSk = args.eth_privkey
+    }
+
+    if (!validateEthSk(ethSk)) {
+        console.error('Error: invalid Ethereum private key')
+        return
+    }
+
+    if (! (await checkDeployerProviderConnection(ethSk, ethProvider))) {
+        console.error('Error: unable to connect to the Ethereum provider at', ethProvider)
+        return
+    }
+
+    const provider = new providers.JsonRpcProvider(ethProvider)
+    const wallet = new Wallet(ethSk, provider)
+
+    if (! await contractExists(provider, unirepAddress)) {
+        console.error('Error: there is no contract deployed at the specified address')
+        return
+    }
+
+    const unirepContract = new Contract(
+        unirepAddress,
+        Unirep.abi,
+        wallet,
+    )
+
+    // Fast-forward to end of epoch if in test environment
+    if (args.is_test) {
+        const epochLength = (await unirepContract.epochLength()).toNumber()
+        await provider.send("evm_increaseTime", [epochLength])
+    }
+
+    const currentEpoch = await unirepContract.currentEpoch()
+    let tx
+    try {
+        const numEpochKeysToSeal = await unirepContract.getNumEpochKey(currentEpoch)
+        console.log(currentEpoch, numEpochKeysToSeal)
+        tx = await unirepContract.beginEpochTransition(
+            numEpochKeysToSeal,
+            { gasLimit: 9000000 }
+        )
+
+    } catch(e) {
+        console.error('Error: the transaction failed')
+        if (e.message) {
+            console.error(e.message)
+        }
+        return
+    }
+
+    console.log('Transaction hash:', tx.hash)
+    console.log('End of epoch:', currentEpoch.toString())
+}
+
+export {
+    epochTransition,
+    configureSubparser,
+}
