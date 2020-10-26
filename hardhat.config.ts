5b1f83b078f95101bc74c4946c4b7fc86fadc57b
NIC619 <twedusuck@gmail.com>
2020-10-26 18:38:47 +0800

    Migrate buidler to hardhat: update configs

---
+++ hardhat.config.ts
@@ -0,0 +1,23 @@
+import { HardhatUserConfig } from "hardhat/config"
+import "@nomiclabs/hardhat-ethers"
+import "@nomiclabs/hardhat-waffle"
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      blockGasLimit: 12000000
+    },
+    local: {
+      url: "http://localhost:8545"
+    },
+  },
+  solidity: {
+    version: "0.6.8",
+    settings: {
+      optimizer: { enabled: true, runs: 200 }
+    }
+  },
+}
+
+export default config;
