fb3710bbfe1eb52682fa3b958ee180e90e236ec8
NIC619 <twedusuck@gmail.com>
2020-10-27 10:05:20 +0800

    Rebuilt poseidon after `npx hardhat node`: `npx hardhat node` would enforce compiling the contracts again

--- scripts/testCLI.sh
+++ scripts/testCLI.sh
@@ -4,4 +4,5 @@ cd "$(dirname "$0")"
 cd ..
 
 npx hardhat node &
-sleep 3 && npx hardhat --network local test cli/test/testAllCommands.ts
+# npx hardhat node would enforce compiling the contracts so Poseidon contracts need to be rebuilt
+sleep 3 && npx ts-node scripts/buildPoseidon.ts && npx hardhat --network local --no-compile test cli/test/testAllCommands.ts
