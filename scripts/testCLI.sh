607bffcf79f72a8730f1b3a28600514c788ab77f
NIC619 <twedusuck@gmail.com>
2020-10-27 11:56:47 +0800

    Fix no compile flag and extend sleep time

--- scripts/testCLI.sh
+++ scripts/testCLI.sh
@@ -4,5 +4,6 @@ cd "$(dirname "$0")"
 cd ..
 
 npx hardhat node &
+sleep 6
 # npx hardhat node would enforce compiling the contracts so Poseidon contracts need to be rebuilt
-sleep 3 && npx ts-node scripts/buildPoseidon.ts && npx hardhat --network local --no-compile test cli/test/testAllCommands.ts
+npx ts-node scripts/buildPoseidon.ts && npx hardhat --network local test --no-compile cli/test/testAllCommands.ts
