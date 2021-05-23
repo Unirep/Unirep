e905913c46f87900efe78b154a5a6841daf1c936
NIC619 <twedusuck@gmail.com>
2021-05-23 17:02:53 +0800

    Upgrade solidity compiler to 0.7.6

--- contracts/Unirep.sol
+++ contracts/Unirep.sol
@@ -1,5 +1,5 @@
-pragma experimental ABIEncoderV2;
-pragma solidity ^0.6.0;
+pragma abicoder v2;
+pragma solidity 0.7.6;
 
 import "@openzeppelin/contracts/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
