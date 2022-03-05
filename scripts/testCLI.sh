6e5dc7774d7542495653503cf5b2f17b948c0aae
Vu Vo <vu.voth@gmail.com>
2022-03-05 13:51:09 +0700

    add missing files

--- scripts/testCLI.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -xe
-
-cd "$(dirname "$0")"
-cd ..
-
-npx hardhat node &
-sleep 6
-# npx hardhat node would enforce compiling the contracts so Poseidon contracts need to be rebuilt
-NODE_OPTIONS=--max-old-space-size=4096 npx hardhat --network local test --no-compile cli/test/testAllCommands.ts
