e424c311111e542b0b83836b1d0afcdaf7fc268d
Vu Vo <vu.voth@gmail.com>
2022-03-26 19:14:54 +0700

    remove unused code

--- packages/circuits/config/index.ts
+++ packages/circuits/config/index.ts
@@ -7,18 +7,6 @@ enum Circuit {
     userStateTransition = 'userStateTransition',
 }
 
-// const numEpochKeyNoncePerEpoch = 3
-
-// const numAttestationsPerProof = 5
-
-// const circuitGlobalStateTreeDepth = 4
-
-// const circuitUserStateTreeDepth = 4
-
-// const circuitEpochTreeDepth = 32
-
-// const maxReputationBudget = 10
-
 const verifyEpochKeyCircuitPath = '../zksnarkBuild/verifyEpochKey_main.circom'
 
 const proveReputationCircuitPath = '../zksnarkBuild/proveReputation_main.circom'
@@ -35,12 +23,6 @@ const userStateTransitionCircuitPath =
 
 export {
     Circuit,
-    // circuitGlobalStateTreeDepth,
-    // circuitUserStateTreeDepth,
-    // circuitEpochTreeDepth,
-    // numEpochKeyNoncePerEpoch,
-    // numAttestationsPerProof,
-    // maxReputationBudget,
     verifyEpochKeyCircuitPath,
     proveReputationCircuitPath,
     proveUserSignUpCircuitPath,
