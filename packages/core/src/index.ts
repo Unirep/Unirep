1afd82da40a4a549505eabf15e4548343e7220e1
vivianjeng <vivi432@yahoo.com.tw>
2022-06-04 01:05:23 +0800

    fix(core): remove `ISettings`

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,7 +2,6 @@ import {
     IReputation,
     IEpochTreeLeaf,
     IUserStateLeaf,
-    ISettings,
     IUnirepState,
     IUserState,
 } from './interfaces'
@@ -15,7 +14,6 @@ import { UnirepProtocol } from './UnirepProtocol'
 import { genUnirepState, genUserState } from './utils'
 export {
     IEpochTreeLeaf,
-    ISettings,
     IUnirepState,
     UnirepState,
     IReputation,
