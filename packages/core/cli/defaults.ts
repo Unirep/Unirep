0ea2d8d05de039bd0c2e41f7773ac6d2006bbc87
vivianjeng <vivi432@yahoo.com.tw>
2022-06-04 00:46:22 +0800

    chore(core): remove config, remove tests from tsconfig

--- packages/core/cli/defaults.ts
+++ packages/core/cli/defaults.ts
@@ -1,7 +1,7 @@
 //  TODO: better setup for cli Ethereum client.
 // import { ALCHEMY_API_KEY } from '../config/privateKey'
 
-import { EPOCH_LENGTH } from '@unirep/circuits/config'
+// import { EPOCH_LENGTH } from '@unirep/circuits/config'
 import { ethers } from 'ethers'
 
 // apply the api key from https://www.alchemy.com/
@@ -9,7 +9,7 @@ import { ethers } from 'ethers'
 // const DEFAULT_ETH_PROVIDER = `https://goerli.infura.io/v3/${INFURA_API_KEY}`
 const DEFAULT_ETH_PROVIDER = 'http://localhost:8545'
 const DEFAULT_START_BLOCK = 0
-const DEFAULT_EPOCH_LENGTH = EPOCH_LENGTH
+const DEFAULT_EPOCH_LENGTH = 600
 const DEFAULT_ATTESTING_FEE = ethers.utils.parseEther('0')
 
 const DEFAULT_TREE_DEPTHS_CONFIG = 'circuit'
