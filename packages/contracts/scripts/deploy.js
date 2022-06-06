b24117af62621236aa1fec2a9de99004d4d8edbd
vivianjeng <vivi432@yahoo.com.tw>
2022-06-06 11:13:56 +0800

    feat(contracts): add `yarn deploy`

---
+++ packages/contracts/scripts/deploy.js
@@ -0,0 +1,24 @@
+// @ts-ignore
+const { ethers } = require('hardhat')
+const UnirepCircuit = require('@unirep/circuits')
+const path = require('path')
+const UnirepContract = require('../src')
+
+const zkFilesPath = path.join(__dirname, '../../circuits/zksnarkBuild')
+const artifactsPath = path.join(__dirname, '../../contracts/build/artifacts')
+const circuitConfig = UnirepCircuit.default.getConfig(zkFilesPath)
+
+;(async () => {
+    const [signer] = await ethers.getSigners()
+    const config = {
+        attestingFee: ethers.utils.parseEther('0.1'),
+        epochLength: 30,
+        maxUsers: 10,
+        maxAttesters: 10,
+        ...circuitConfig,
+    }
+    const unirep = await UnirepContract.default.deploy(artifactsPath, signer, config)
+    await unirep.deployed()
+    console.log(`Unirep address: ${unirep.address}`)
+    return 0
+})()
