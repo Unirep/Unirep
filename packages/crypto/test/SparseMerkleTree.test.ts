03457a9ad2b9131c9012ba428e062083e9df41b5
vivianjeng <vivi432@yahoo.com.tw>
2022-06-09 13:16:13 +0800

    test(crypto): add crypto test scripts

---
+++ packages/crypto/test/SparseMerkleTree.test.ts
@@ -0,0 +1,31 @@
+import { expect } from 'chai'
+import Keyv from 'keyv'
+import { poseidon } from "circomlibjs"
+import { SparseMerkleTree } from '../src'
+
+describe('SparseMerkleTree', function () {
+    let tree: SparseMerkleTree
+    const depth = 4
+    const zeroHash = BigInt(0)
+
+    it('constructor', async () => {
+        tree = new SparseMerkleTree(poseidon, new Keyv(), depth, zeroHash)
+    })
+
+    it('update', async () => {
+        const rootBeforeInsert = tree.root
+        const leafKey = BigInt(2)
+        const leafValue = BigInt(3)
+        await tree.update(leafKey, leafValue)
+        expect(tree.root).not.equal(rootBeforeInsert)
+    })
+
+    it('genProof/verifyProof', async () => {
+        for (let index = 0; index < depth; index++) {
+            const leafKey = BigInt(index)
+            const proof = await tree.createProof(leafKey)
+            const isValid = await tree.verifyProof(leafKey, proof)
+            expect(isValid).to.be.true
+        }
+    })
+})
