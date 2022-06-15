f69b39c6011ae80cd2cb868f8da0eea594ab8cff
Chance <jchancehud@gmail.com>
2022-06-14 21:17:57 -0500

    fix: add typescript as dev dep to allow build (#54)
    fix: contracts prepare script

--- packages/contracts/scripts/prepare.js
+++ packages/contracts/scripts/prepare.js
@@ -22,8 +22,6 @@ fs.copyFileSync(
     path.join(__dirname, '../build/README.md')
 )
 
-fs.rmSync(path.join(__dirname, '../build/test'), { recursive: true })
-
 // copy files from contracts recursively
 function copyContracts(currentDir, outPath) {
     if (!path.isAbsolute(currentDir)) throw new Error('Path is not absolute')
