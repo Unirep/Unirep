6bb0f8cf72ac98573a6c302ea0af1b6d77a72fd4
Chance Hudson <jchancehud@gmail.com>
2022-07-10 21:50:07 -0500

    fix(contracts): only put abi in file

--- packages/contracts/scripts/abi.js
+++ packages/contracts/scripts/abi.js
@@ -1,11 +1,9 @@
 const fs = require('fs')
 const path = require('path')
 
-const ABI = require('../build/artifacts/contracts/Unirep.sol/Unirep.json')
-ABI.bytecode = ''
-ABI.deployedBytecode = ''
+const { abi } = require('../build/artifacts/contracts/Unirep.sol/Unirep.json')
 
 fs.writeFileSync(
     path.join(__dirname, '../abi/Unirep.json'),
-    JSON.stringify(ABI)
+    JSON.stringify(abi)
 )
