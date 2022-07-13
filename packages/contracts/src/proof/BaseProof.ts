f31705e369dc23d1a0ca0b6e45800fd6730c6b30
Chance <jchancehud@gmail.com>
2022-07-12 23:38:38 -0500

    feat(contracts): allow proof class to be initialized with snarkproof or bigint arrray (#75)

--- packages/contracts/src/proof/BaseProof.ts
+++ packages/contracts/src/proof/BaseProof.ts
@@ -1,13 +1,14 @@
 import {
     Circuit,
     formatProofForVerifierContract,
+    formatProofForSnarkjsVerification,
     Prover,
 } from '@unirep/circuits'
 import { SnarkProof } from '@unirep/crypto'
 import { ethers, BigNumberish } from 'ethers'
 
 export class BaseProof {
-    private _snarkProof: SnarkProof
+    readonly _snarkProof: SnarkProof
     protected circuit?: Circuit
 
     readonly publicSignals: BigNumberish[]
@@ -16,12 +17,23 @@ export class BaseProof {
 
     constructor(
         publicSignals: BigNumberish[],
-        proof: SnarkProof,
+        proof: SnarkProof | BigNumberish[],
         prover?: Prover
     ) {
-        this._snarkProof = proof
-        const formattedProof: any[] = formatProofForVerifierContract(proof)
-        this.proof = formattedProof
+        if (Array.isArray(proof)) {
+            // assume it's formatted for verifier contract
+            this.proof = proof
+            this._snarkProof = formatProofForSnarkjsVerification(
+                proof.map((p) => p.toString())
+            )
+        } else if (typeof proof === 'object') {
+            // assume it's a SnarkProof
+            const formattedProof: any[] = formatProofForVerifierContract(proof)
+            this._snarkProof = proof
+            this.proof = formattedProof
+        } else {
+            throw new Error('Invalid proof supplied')
+        }
         this.publicSignals = publicSignals
         this.prover = prover
     }
