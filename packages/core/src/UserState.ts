7ce4f5ba77be0c96a9faee23c773762fbf8a7468
Chance Hudson <jchancehud@gmail.com>
2022-07-26 01:16:01 -0500

    refactor(core): index db, switch bool to int
    fix: attestation index int -> string
    
    fix: primary key for upsert

--- packages/core/src/UserState.ts
+++ packages/core/src/UserState.ts
@@ -217,6 +217,7 @@ export default class UserState extends Synchronizer {
         const attestations = await this._db.findMany('Attestation', {
             where: {
                 OR: orConditions,
+                valid: 1,
             },
             orderBy: {
                 index: 'asc',
@@ -277,7 +278,7 @@ export default class UserState extends Synchronizer {
         return this._db.findMany('Attestation', {
             where: {
                 epochKey,
-                valid: true,
+                valid: 1,
             },
         })
     }
@@ -301,7 +302,7 @@ export default class UserState extends Synchronizer {
             where: {
                 event: 'IndexedUserStateTransitionProof',
                 index,
-                valid: true,
+                valid: 1,
             },
         })
     }
@@ -374,11 +375,12 @@ export default class UserState extends Synchronizer {
                 )
             allEpks.push(...epks)
         }
+        if (allEpks.length === 0) return r
         const attestations = await this._db.findMany('Attestation', {
             where: {
                 epochKey: allEpks,
                 attesterId: Number(attesterId),
-                valid: true,
+                valid: 1,
             },
             orderBy: {
                 index: 'asc',
