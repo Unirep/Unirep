f8747a7f9a0d5729cae92d27b45f04ab730ac581
Chance <jchancehud@gmail.com>
2022-07-30 20:18:18 -0500

    fix: signup as int

--- packages/core/src/Synchronizer.ts
+++ packages/core/src/Synchronizer.ts
@@ -818,7 +818,7 @@ export class Synchronizer extends EventEmitter {
             posRep: Number(decodedData.attestation.posRep),
             negRep: Number(decodedData.attestation.negRep),
             graffiti: decodedData.attestation.graffiti.toString(),
-            signUp: Boolean(Number(decodedData.attestation?.signUp)),
+            signUp: Number(decodedData.attestation?.signUp),
             hash: attestation.hash().toString(),
             valid: !!rootExists,
         })
@@ -875,7 +875,7 @@ export class Synchronizer extends EventEmitter {
             posRep: Number(decodedData.attestation.posRep),
             negRep: Number(decodedData.attestation.negRep),
             graffiti: decodedData.attestation.graffiti.toString(),
-            signUp: Boolean(Number(decodedData.attestation?.signUp)),
+            signUp: Number(decodedData.attestation?.signUp),
             hash: attestation.hash().toString(),
         })
 
