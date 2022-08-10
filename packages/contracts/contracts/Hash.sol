5a73eec6f93600a4287608a2b1e4a8c87db8fb12
Chance <jchancehud@gmail.com>
2022-08-10 15:10:45 -0500

    feat(contracts): solidity smt implementation

---
+++ packages/contracts/contracts/Hash.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library Poseidon2 {
+    function poseidon(uint256[2] memory) public pure returns (uint256) {}
+}
+
+library Poseidon3 {
+    function poseidon(uint256[3] memory) public pure returns (uint256) {}
+}
+
+library Poseidon5 {
+    function poseidon(uint256[5] memory) public pure returns (uint256) {}
+}
---
+++ packages/contracts/contracts/test/SparseMerkleTreeTest.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {SparseMerkleTree, SparseTreeData} from '../SparseMerkleTree.sol';
+import {Poseidon2} from '../Hash.sol';
+
+contract SparseMerkleTreeTest {
+    SparseTreeData public smt;
+
+    constructor(uint256 depth) {
+        SparseMerkleTree.init(smt, depth, 0);
+    }
+
+    function root() public view returns (uint256) {
+        return smt.root;
+    }
+
+    function insert(uint256 index, uint256 leaf) public {
+        SparseMerkleTree.insert(smt, index, leaf);
+    }
+}
