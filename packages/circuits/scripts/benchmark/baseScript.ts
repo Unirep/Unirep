f830f493743dc0d68fa7ceb81cd6a60f989dc933
Vu Vo <vu.voth@gmail.com>
2022-08-14 17:05:10 +0700

    finish basescript

--- packages/circuits/scripts/benchmark/baseScript.ts
+++ packages/circuits/scripts/benchmark/baseScript.ts
@@ -5,15 +5,14 @@
 
 import { mkdtempSync, writeFileSync } from 'fs'
 import * as fastFile from 'fastfile'
-import path, { resolve } from 'path'
+import path from 'path'
 import * as circom from 'circom'
 import * as snarkjs from 'snarkjs'
-import * as crypto from '@unirep/crypto'
 
 async function executeTimeOf(
     name: string,
     circomComponent: string,
-    inputs: any
+    genInputFn: any
 ): Promise<bigint> {
     // create tmp folder to store circom compile files.
     const tmpPath = mkdtempSync('/tmp/unirep')
@@ -37,18 +36,34 @@ async function executeTimeOf(
     // compile circom file
     await circom.compiler(circomPath, options)
 
-    return Promise.resolve(BigInt(0))
-}
+    const ptau = path.join(
+        __dirname,
+        '../../zksnarkBuild/powersOfTau28_hez_final_17.ptau'
+    )
 
-const tmpContent = `
-template Equal() {
-    signal input a;
-    signal output b;
-    a === b;
-}
-component main = Equal();
-`
+    await snarkjs.zKey.newZKey(r1cs, ptau, zkey)
+    const vkeyJson = await snarkjs.zKey.exportVerificationKey(zkey)
 
-executeTimeOf('tmp', tmpContent, undefined)
-    .then((ans) => console.log(ans))
-    .catch((e) => console.log(e))
+    const NUMBER_CASE = 10
+    let totalTime = 0
+    for (let no = -1; no <= NUMBER_CASE; ++no) {
+        const inputs = genInputFn()
+        const start = performance.now()
+        const { proof, publicSignals } = await snarkjs.groth16.fullProve(
+            inputs,
+            wasmOut,
+            zkey
+        )
+
+        if (no > -1) {
+            totalTime += +performance.now() - start
+        }
+        const isValid = await snarkjs.groth16.verify(
+            vkeyJson,
+            publicSignals,
+            proof
+        )
+        if (!isValid) throw new Error('invalid')
+    }
+    return Promise.resolve(BigInt(Math.floor(totalTime / NUMBER_CASE)))
+}
