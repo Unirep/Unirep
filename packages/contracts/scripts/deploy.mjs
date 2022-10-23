280b443208e86c75322fedda01ec5cb3bb1df7b1
Chance <jchancehud@gmail.com>
2022-10-23 13:00:44 -0500

    fix(contracts): deploy using default settings

--- packages/contracts/scripts/deploy.mjs
+++ packages/contracts/scripts/deploy.mjs
@@ -1,17 +1,5 @@
 import { deployUnirep } from '../build/deploy/index.js'
-import { STATE_TREE_DEPTH, EPOCH_TREE_DEPTH } from '@unirep/circuits'
-
-const attestingFee = ethers.utils.parseEther('0.000000000001')
-const numEpochKeyNoncePerEpoch = 3
-const numAttestationsPerProof = 5
-const epochLength = 12 * 60 * 60 // seconds
-const maxReputationBudget = 10
 
 const [signer] = await ethers.getSigners()
-const unirep = await deployUnirep(signer, {
-    attestingFee,
-    numEpochKeyNoncePerEpoch,
-    stateTreeDepth: STATE_TREE_DEPTH,
-    epochTreeDepth: EPOCH_TREE_DEPTH,
-})
+const unirep = await deployUnirep(signer)
 console.log(`Unirep address: ${unirep.address}`)
