f8e395f7a8402f380d81c4784611a09074623901
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2022-10-29 14:44:37 +0800

    test(contracts): attester sign up via relayer

--- packages/contracts/test/attesterSignup.test.ts
+++ packages/contracts/test/attesterSignup.test.ts
@@ -1,7 +1,7 @@
 // @ts-ignore
 import { ethers } from 'hardhat'
 import { expect } from 'chai'
-import { IncrementalMerkleTree } from '@unirep/crypto'
+import { genRandomSalt, IncrementalMerkleTree } from '@unirep/crypto'
 import { STATE_TREE_DEPTH } from '@unirep/circuits'
 
 import { EPOCH_LENGTH } from '../src'
@@ -35,6 +35,79 @@ describe('Attester Signup', function () {
         await unirepContract
             .connect(attester)
             .attesterSignUp(EPOCH_LENGTH)
+            .then((t) => t.wait())
+
+        const currentEpoch = await unirepContract.attesterCurrentEpoch(
+            attester.address
+        )
+        expect(currentEpoch.toString()).to.equal('0')
+        const epochTimeRemaining =
+            await unirepContract.attesterEpochRemainingTime(attester.address)
+        expect(epochTimeRemaining.toNumber()).to.be.lte(+EPOCH_LENGTH)
+        const epochLength = await unirepContract.attesterEpochLength(
+            attester.address
+        )
+        expect(epochLength.toNumber()).to.equal(EPOCH_LENGTH)
+
+        const stateTree = new IncrementalMerkleTree(STATE_TREE_DEPTH)
+        const exists = await unirepContract.attesterStateTreeRootExists(
+            attester.address,
+            currentEpoch,
+            stateTree.root
+        )
+        expect(exists).to.be.true
+        const stateRoot = await unirepContract.attesterStateTreeRoot(
+            attester.address,
+            currentEpoch
+        )
+        expect(stateRoot).to.equal(stateTree.root)
+        const semaphoreRoot = await unirepContract.attesterSemaphoreGroupRoot(
+            attester.address
+        )
+        expect(semaphoreRoot).to.equal(stateTree.root)
+        for (let x = 1; x < 10; x++) {
+            await ethers.provider.send('evm_increaseTime', [EPOCH_LENGTH])
+            await ethers.provider.send('evm_mine', [])
+            const _currentEpoch = await unirepContract.attesterCurrentEpoch(
+                attester.address
+            )
+            expect(_currentEpoch.toString()).to.equal(x.toString())
+        }
+    })
+
+    it('should fail to signup attester via relayer if signature is not valid', async () => {
+        const accounts = await ethers.getSigners()
+        const attester = accounts[3]
+        const relayer = accounts[4]
+
+        const wrongMessage = genRandomSalt()
+        const signature = await attester.signMessage(wrongMessage)
+        await expect(
+            unirepContract
+                .connect(relayer)
+                .attesterSignUpViaRelayer(
+                    attester.address,
+                    EPOCH_LENGTH,
+                    signature
+                )
+        ).to.be.revertedWithCustomError(unirepContract, 'InvalidSignature')
+    })
+
+    it('should signup attester via relayer', async () => {
+        const accounts = await ethers.getSigners()
+        const attester = accounts[3]
+        const relayer = accounts[4]
+
+        const message = ethers.utils.solidityKeccak256(
+            ['address', 'address'],
+            [attester.address, unirepContract.address]
+        )
+        const signature = await attester.signMessage(
+            ethers.utils.arrayify(message)
+        )
+        await unirepContract
+            .connect(relayer)
+            .attesterSignUpViaRelayer(attester.address, EPOCH_LENGTH, signature)
             .then((t) => t.wait())
 
         const currentEpoch = await unirepContract.attesterCurrentEpoch(
