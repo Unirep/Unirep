3e445914f42b39a8dfc7c1fc07d3feb40243dc41
Chance <jchancehud@gmail.com>
2023-01-29 10:51:45 -0600

    chore(circuits): clean up config file

--- packages/circuits/config/index.ts
+++ packages/circuits/config/index.ts
@@ -8,16 +8,12 @@ export const STATE_TREE_DEPTH = 12
 export const EPOCH_TREE_DEPTH = 4
 export const EPOCH_TREE_ARITY = 3
 
+export const SNARK_SCALAR_FIELD =
+    '21888242871839275222246405745257275088548364400416034343698204186575808495617'
+
 export const R = hash1([
     `0x${Buffer.from('unirep_polyhash_constant', 'utf8').toString('hex')}`,
 ])
-
-export const defaultEpochTreeLeaf = hash4([0, 0, 0, 0])
-const smt = new SparseMerkleTree(EPOCH_TREE_DEPTH, defaultEpochTreeLeaf)
-export const EMPTY_EPOCH_TREE_ROOT = smt.root.toString()
-
-export const SNARK_SCALAR_FIELD =
-    '21888242871839275222246405745257275088548364400416034343698204186575808495617'
 
 //~~ Calculate the R values and export
 const _R = new BN(R.toString(), 10)
