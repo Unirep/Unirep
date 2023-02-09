d910b6541404f2eab7a89f30fae08c29247c3904
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2023-02-09 14:58:50 +0800

    test(core): epochKeyProof, reputationProof tests

---
+++ packages/core/test/reputationProof.test.ts
@@ -0,0 +1,138 @@
+// @ts-ignore
+import { ethers } from 'hardhat'
+import { expect } from 'chai'
+import { ZkIdentity } from '@unirep/utils'
+import { EPOCH_LENGTH } from '@unirep/contracts'
+import { deployUnirep } from '@unirep/contracts/deploy'
+
+import { genUserState } from './utils'
+
+describe('Reputation proof', function () {
+    this.timeout(0)
+    let unirepContract
+
+    before(async () => {
+        const accounts = await ethers.getSigners()
+        unirepContract = await deployUnirep(accounts[0])
+        const attester = accounts[1]
+        await unirepContract
+            .connect(attester)
+            .attesterSignUp(EPOCH_LENGTH)
+            .then((t) => t.wait())
+    })
+
+    {
+        let snapshot
+        beforeEach(async () => {
+            snapshot = await ethers.provider.send('evm_snapshot', [])
+        })
+
+        afterEach(async () => {
+            await ethers.provider.send('evm_revert', [snapshot])
+        })
+    }
+
+    it('should generate a zero reputation proof', async () => {
+        const accounts = await ethers.getSigners()
+        const attester = accounts[1]
+        const attesterId = BigInt(attester.address)
+        const id = new ZkIdentity()
+        const userState = await genUserState(
+            ethers.provider,
+            unirepContract.address,
+            id,
+            attesterId
+        )
+        const epoch = await userState.loadCurrentEpoch()
+        {
+            const { publicSignals, proof } = await userState.genUserSignUpProof(
+                { epoch }
+            )
+            await unirepContract
+                .connect(attester)
+                .userSignUp(publicSignals, proof)
+                .then((t) => t.wait())
+        }
+        await userState.waitForSync()
+        const proveZeroRep = true
+        const proof = await userState.genProveReputationProof({
+            proveZeroRep
+        })
+
+        const valid = await proof.verify()
+        expect(valid).to.be.true
+        await userState.stop()
+    })
+
+    it('should reveal epoch key nonce', async () => {
+        const accounts = await ethers.getSigners()
+        const attester = accounts[1]
+        const attesterId = BigInt(attester.address)
+        const id = new ZkIdentity()
+        const userState = await genUserState(
+            ethers.provider,
+            unirepContract.address,
+            id,
+            attesterId
+        )
+        const epoch = await userState.loadCurrentEpoch()
+        {
+            const { publicSignals, proof } = await userState.genUserSignUpProof(
+                { epoch }
+            )
+            await unirepContract
+                .connect(attester)
+                .userSignUp(publicSignals, proof)
+                .then((t) => t.wait())
+        }
+        await userState.waitForSync()
+        const epkNonce = 1
+        const revealNonce = true
+        const proof = await userState.genProveReputationProof({
+            epkNonce,
+            revealNonce
+        })
+
+        const valid = await proof.verify()
+        expect(valid).to.be.true
+        expect(proof.nonce).to.equal(epkNonce)
+        await userState.stop()
+    })
+
+    it('should not reveal epoch key nonce', async () => {
+        const accounts = await ethers.getSigners()
+        const attester = accounts[1]
+        const attesterId = BigInt(attester.address)
+        const id = new ZkIdentity()
+        const userState = await genUserState(
+            ethers.provider,
+            unirepContract.address,
+            id,
+            attesterId
+        )
+        const epoch = await userState.loadCurrentEpoch()
+        {
+            const { publicSignals, proof } = await userState.genUserSignUpProof(
+                { epoch }
+            )
+            await unirepContract
+                .connect(attester)
+                .userSignUp(publicSignals, proof)
+                .then((t) => t.wait())
+        }
+        await userState.waitForSync()
+        const epkNonce = 1
+        const revealNonce = false
+        const proof = await userState.genProveReputationProof({
+            epkNonce,
+            revealNonce
+        })
+
+        const valid = await proof.verify()
+        expect(valid).to.be.true
+        expect(proof.nonce).to.equal('0')
+        await userState.stop()
+    })
+
+    // TODO: should prove minRep, maxRep, graffiti
+})
