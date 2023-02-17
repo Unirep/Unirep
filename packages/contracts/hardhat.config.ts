e29ab019dcb73c6a3c28bf971093ab92ae961856
Chance <jchancehud@gmail.com>
2023-02-17 01:35:32 -0600

    chore(contracts): enable optimizer

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -23,7 +23,7 @@ const config: HardhatUserConfig = {
             {
                 version: '0.8.6',
                 settings: {
-                    optimizer: { enabled: true, runs: 200 },
+                    optimizer: { enabled: true, runs: 999999999 },
                 },
             },
         ],
