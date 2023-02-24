3c294918f866c288a69421b62ee49a6500162082
Chance <jchancehud@gmail.com>
2023-02-23 20:35:09 -0600

    feat(contracts): history root getter function

--- packages/contracts/contracts/Unirep.sol
+++ packages/contracts/contracts/Unirep.sol
@@ -759,6 +759,14 @@ contract Unirep is IUnirep, VerifySignature {
             attester.state[epoch].polysum.hash == 0;
     }
 
+    function attesterHistoryRootExists(uint160 attesterId, uint256 root)
+        public
+        view
+        returns (bool)
+    {
+        return attesters[attesterId].historyTreeRoots[root];
+    }
+
     function attesterStateTreeRootExists(
         uint160 attesterId,
         uint256 epoch,
--- packages/contracts/test/accessors.test.ts
+++ packages/contracts/test/accessors.test.ts
@@ -1,9 +1,16 @@
 // @ts-ignore
 import { ethers } from 'hardhat'
 import { expect } from 'chai'
-import { ZkIdentity, stringifyBigInts } from '@unirep/utils'
-import { Circuit, SignupProof } from '@unirep/circuits'
+import {
+    ZkIdentity,
+    stringifyBigInts,
+    hash2,
+    IncrementalMerkleTree,
+} from '@unirep/utils'
+import { Circuit, CircuitConfig, SignupProof } from '@unirep/circuits'
 import { defaultProver } from '@unirep/circuits/provers/defaultProver'
+
+const { HISTORY_TREE_DEPTH, STATE_TREE_DEPTH } = CircuitConfig.default
 
 import { EPOCH_LENGTH } from '../src'
 import { deployUnirep } from '../deploy'
@@ -69,6 +76,57 @@ describe('Attester getters', function () {
                 attester.address
             )
             expect(numberOfUsers).to.equal(x)
+        }
+    })
+
+    it('should get history root existence', async () => {
+        const accounts = await ethers.getSigners()
+        const attester = accounts[1]
+        const epoch = await unirepContract.attesterCurrentEpoch(
+            attester.address
+        )
+        const id = new ZkIdentity()
+        const r = await defaultProver.genProofAndPublicSignals(
+            Circuit.signup,
+            stringifyBigInts({
+                epoch,
+                identity_nullifier: id.identityNullifier,
+                identity_trapdoor: id.trapdoor,
+                attester_id: attester.address,
+            })
+        )
+        const { publicSignals, proof, stateTreeLeaf } = new SignupProof(
+            r.publicSignals,
+            r.proof,
+            defaultProver
+        )
+        await unirepContract
+            .connect(attester)
+            .userSignUp(publicSignals, proof)
+            .then((t) => t.wait())
+        await ethers.provider.send('evm_increaseTime', [EPOCH_LENGTH])
+        await unirepContract
+            .connect(accounts[4])
+            .sealEmptyEpoch(epoch, attester.address)
+            .then((t) => t.wait())
+        await ethers.provider.send('evm_mine', [])
+        const stateTree = new IncrementalMerkleTree(STATE_TREE_DEPTH)
+        stateTree.insert(stateTreeLeaf)
+        const historyTree = new IncrementalMerkleTree(HISTORY_TREE_DEPTH)
+        historyTree.insert(hash2([stateTree.root, 0]))
+        {
+            const exists = await unirepContract.attesterHistoryRootExists(
+                attester.address,
+                historyTree.root
+            )
+            expect(exists).to.be.true
+        }
+        {
+            const exists = await unirepContract.attesterHistoryRootExists(
+                attester.address,
+                historyTree.root + BigInt(1)
+            )
+            expect(exists).to.be.false
         }
     })
 })
