8263d35774aa61075e44a3f1df371d1216fbfe80
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2023-03-21 17:22:03 +0700

    feat(core): update hardhat compiler

--- packages/core/hardhat.config.ts
+++ packages/core/hardhat.config.ts
@@ -16,7 +16,7 @@ const config: HardhatUserConfig = {
     solidity: {
         compilers: [
             {
-                version: '0.8.0',
+                version: '0.8.17',
                 settings: {
                     optimizer: { enabled: true, runs: 200 },
                 },
