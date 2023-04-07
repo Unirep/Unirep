441233a454b7fb88cc98f9484b36312f99c77087
Chance <jchancehud@gmail.com>
2023-04-07 00:17:49 -0500

    chore: remove subgraph

--- packages/subgraph/scripts/copyABI.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-const Unirep = require('@unirep/contracts/abi/Unirep.json')
-
-const abiPath = './abis/Unirep.json'
-try {
-    fs.mkdirSync('./abis')
-    fs.writeFileSync(abiPath, JSON.stringify(Unirep))
-} catch (_) {}
--- packages/subgraph/src/utils.ts
+++
@@ -1,5 +0,0 @@
-import { ethereum } from '@graphprotocol/graph-ts'
-
-export function buildId(event: ethereum.Event): string {
-    return event.transaction.hash.toHexString() + event.logIndex.toString()
-}
