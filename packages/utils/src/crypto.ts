34127ca60a5565a158db24f8e6e848c82f660d04
YURIKO NISHIJIMA <yuriko.nishijima@gmail.com>
2023-04-12 08:43:30 -0400

    fix rebase again

--- packages/utils/src/crypto.ts
+++ packages/utils/src/crypto.ts
@@ -8,31 +8,6 @@ export const F = BigInt(SNARK_SCALAR_FIELD)
 
 export const MAX_EPOCH = 2 ** 48 - 1
 
-// Ordered merkle tree polysum constant
-// export const OMT_R = poseidon([
-//     BigInt(
-//         `0x${Buffer.from('unirep_omt_polysum_constant', 'utf8').toString(
-//             'hex'
-//         )}`
-//     ),
-// ])
-// export a precalculated value to avoid the hash on import
-export const OMT_R = BigInt(
-    '19840472963655813647419884432877523255831900116552197704230384899846353674447'
-)
-
-// Epoch tree leaf polysum constant
-// export const EPK_R = poseidon([
-//     BigInt(
-//         `0x${Buffer.from('unirep_epk_polysum_constant', 'utf8').toString(
-//             'hex'
-//         )}`
-//     ),
-// ])
-export const EPK_R = BigInt(
-    '11105707062209303735980536775061420040143715723438319441848723820903914190159'
-)
-
 export const genRandomSalt = () => randomf(F)
 
 export const modexp = (v: bigint, p: number): bigint => {
@@ -80,54 +55,26 @@ export const genEpochKey = (
     return hash4([identitySecret, BigInt(attesterId), epoch, BigInt(nonce)])
 }
 
-export const genUserStateTransitionNullifier = (
-    identitySecret: bigint,
-    attesterId: bigint | string,
-    epoch: number | bigint | number
-): bigint => {
-    return hash3([BigInt(attesterId), BigInt(epoch), identitySecret])
-}
-
-export const genEpochNullifier = (
-    ...args: [
-        identitySecret: bigint,
-        attesterId: bigint | string,
-        epoch: number | bigint | number
-    ]
-) => {
-    console.warn('@unirep/utils:genEpochNullifier is deprecated')
-    console.warn('Use @unirep/utils:genUserStateTransitionNullifier instead')
-    return genUserStateTransitionNullifier(...args)
-}
-
 export const genStateTreeLeaf = (
     idSecret: bigint,
     attesterId: bigint | string,
     epoch: bigint | number,
     data: (bigint | string | number)[]
 ): bigint => {
-    const hashedData = data.map((d) => hash1([d]))
-    let polysum = BigInt(0)
-    for (let x = 0; x < data.length; x++) {
-        const term =
-            (BigInt(hashedData[x]) * modexp(EPK_R, x + 1)) %
-            BigInt(SNARK_SCALAR_FIELD)
-        polysum = (polysum + term) % BigInt(SNARK_SCALAR_FIELD)
+    let hashchain = BigInt(0)
+    for (const d of data) {
+        hashchain = hash2([hashchain, d])
     }
-    return hash4([idSecret, BigInt(attesterId), BigInt(epoch), polysum])
+    return hash4([idSecret, BigInt(attesterId), BigInt(epoch), hashchain])
 }
 
 export const genEpochTreeLeaf = (
     epochKey: bigint | string,
     data: (bigint | string | number)[]
 ) => {
-    const hashedData = data.map((d) => hash1([d]))
-    let polysum = (hash1([epochKey]) * EPK_R) % BigInt(SNARK_SCALAR_FIELD)
-    for (let x = 0; x < data.length; x++) {
-        const term =
-            (BigInt(hashedData[x]) * modexp(EPK_R, x + 2)) %
-            BigInt(SNARK_SCALAR_FIELD)
-        polysum = (polysum + term) % BigInt(SNARK_SCALAR_FIELD)
+    let hashchain = epochKey
+    for (const d of data) {
+        hashchain = hash2([hashchain, d])
     }
-    return polysum
+    return hashchain
 }
