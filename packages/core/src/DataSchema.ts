38cc96f4380eefe004b99760e187cca29a9190e6
chndr1 <bhargav.annem@gmail.com>
2023-04-30 20:26:14 -0500

    fix: less stateful data schema

---
+++ packages/core/src/DataSchema.ts
@@ -0,0 +1,118 @@
+import { CircuitConfig } from '@unirep/circuits'
+
+export class DataSchema {
+    public schema: any[]
+    public config: CircuitConfig
+    public data: bigint[]
+
+    constructor(schema, config = CircuitConfig.default) {
+        this.config = config
+        this.schema = this.parseSchema(schema)
+        this.data = new Array(config.FIELD_COUNT).fill(BigInt(0))
+    }
+
+    parseSchema(schema: any[]): any[] {
+        let sumOffset = 0
+        let replOffset = 0
+        const maxSumOffset = 253 * this.config.SUM_FIELD_COUNT
+        const maxReplOffset =
+            253 * (this.config.FIELD_COUNT - this.config.SUM_FIELD_COUNT)
+
+        return schema.map((field, idx) => {
+            const { name, type, updateBy, ...extranousFields } = field
+            const match = type.match(/^uint(\d+)$/)
+            if (match === null)
+                throw new Error(`Invalid type for field ${name}: "${type}"`)
+            if (Object.keys(extranousFields).length !== 0) {
+                throw new Error(
+                    `Invalid fields included for field ${name}: [${Object.keys(
+                        extranousFields
+                    )}]`
+                )
+            }
+
+            const duplicateEntry = schema.find(
+                (x, i) => x.name == field.name && idx != i
+            )
+            if (duplicateEntry) {
+                throw Error(
+                    `Schema includes a duplicate entry: "${duplicateEntry.name}"`
+                )
+            }
+
+            const bits = +match[1]
+            if (bits < 1 || bits > 253)
+                throw new Error(`Invalid uint size for field ${name}: ${bits}`)
+            if (updateBy === 'sum') {
+                if (
+                    Math.floor(sumOffset / 253) !==
+                    Math.floor((sumOffset + bits - 1) / 253)
+                ) {
+                    sumOffset += 253 - (sumOffset % 253)
+                }
+                if (sumOffset + bits > maxSumOffset) {
+                    throw new Error(
+                        `Invalid schema, field "${name}" exceeds available storage`
+                    )
+                }
+                const dataIndex = Math.floor(sumOffset / 253)
+                const offset = sumOffset % 253
+                sumOffset += bits
+                return { ...field, dataIndex, offset, bits }
+            } else if (updateBy === 'replace') {
+                if (bits !== 253 - this.config.REPL_NONCE_BITS)
+                    throw new Error(
+                        `Invalid uint size for field ${name}: ${bits}`
+                    )
+                if (replOffset + bits > maxReplOffset) {
+                    throw new Error(
+                        `Invalid schema, field "${name}" exceeds available storage`
+                    )
+                }
+
+                const dataIndex =
+                    this.config.SUM_FIELD_COUNT + Math.floor(replOffset / 253)
+                const offset = replOffset % 253
+                replOffset += bits
+                return { ...field, dataIndex, offset, bits }
+            }
+            throw new Error(
+                `Invalid updateBy strategy for field ${name}: "${updateBy}"`
+            )
+        })
+    }
+
+    update(name: string, v: bigint): bigint {
+        const field = this.schema.find((f) => f.name === name)
+        const idx = field.dataIndex
+        const maxVal: bigint = (1n << BigInt(field.bits)) - 1n
+
+        if (field.updateBy === 'sum') {
+            v += (this.data[idx] >> BigInt(field.offset)) & maxVal
+        }
+
+        if (v > maxVal) {
+            throw new Error(`${name} value exceeds allocated storage`)
+        }
+
+        this.data[idx] =
+            (this.data[idx] & ~(maxVal << BigInt(field.offset))) |
+            ((v & maxVal) << BigInt(field.offset))
+
+        return this.data[idx]
+    }
+
+    parseData(data: bigint[]): {} {
+        const parsed = {}
+
+        for (const field of this.schema) {
+            const { name, /* type, updateBy, */ dataIndex, bits, offset } =
+                field
+            parsed[name] =
+                (data[dataIndex] >> BigInt(offset)) &
+                ((1n << BigInt(bits)) - 1n)
+        }
+
+        return parsed
+    }
+}
---
+++ packages/core/test/dataSchema.test.ts
@@ -0,0 +1,303 @@
+// @ts-ignore
+import { expect } from 'chai'
+import { DataSchema } from '../src/DataSchema'
+
+const smallRandom = (x: number) => Math.floor(Math.random() * x)
+
+const schema = [
+    {
+        name: 'posRep',
+        type: 'uint64',
+        updateBy: 'sum',
+    },
+    {
+        name: 'negRep',
+        type: 'uint64',
+        updateBy: 'sum',
+    },
+    {
+        name: 'graffiti',
+        type: 'uint205',
+        updateBy: 'replace',
+    },
+    {
+        name: 'postCount',
+        type: 'uint49',
+        updateBy: 'sum',
+    },
+    {
+        name: 'commentCount',
+        type: 'uint49',
+        updateBy: 'sum',
+    },
+    {
+        name: 'voteCount',
+        type: 'uint49',
+        updateBy: 'sum',
+    },
+]
+
+describe('Check schema parsing', function () {
+    it('should have a valid schema', () => {
+        expect(new DataSchema(schema)).to.not.throw
+    })
+
+    it('fail to parse schema with invalid key', () => {
+        const invalidSchema = [
+            {
+                name: 'posRep',
+                type: 'uint64',
+                updateBy: 'sum',
+            },
+            {
+                name: 'negRep',
+                type: 'uint64',
+                updateBy: 'sum',
+            },
+            {
+                name: 'averageVote',
+                type: 'uint64',
+                updateBy: 'sum',
+                invalidKey: 'invalidValue',
+            },
+        ]
+        expect(() => new DataSchema(invalidSchema)).to.throw(
+            'Invalid fields included for field averageVote: [invalidKey]'
+        )
+    })
+
+    it('fail to parse schema with duplicate entry', () => {
+        const invalidSchema = [
+            {
+                name: 'posRep',
+                type: 'uint64',
+                updateBy: 'sum',
+            },
+            {
+                name: 'posRep',
+                type: 'uint64',
+                updateBy: 'sum',
+            },
+            {
+                name: 'averageVote',
+                type: 'uint64',
+                updateBy: 'sum',
+                invalidKey: 'invalidValue',
+            },
+        ]
+        expect(() => new DataSchema(invalidSchema)).to.throw(
+            'Schema includes a duplicate entry: "posRep"'
+        )
+    })
+
+    it('check schema with invalid type', () => {
+        const invalidSchema = [
+            {
+                name: 'posRep',
+                type: 'uint64',
+                updateBy: 'sum',
+            },
+            {
+                name: 'negRep',
+                type: 'uint8x',
+                updateBy: 'sum',
+            },
+        ]
+        expect(() => new DataSchema(invalidSchema)).to.throw(
+            'Invalid type for field negRep: "uint8x"'
+        )
+    })
+
+    it('fail to use schema to excessive storage', () => {
+        {
+            const invalidSchema = [
+                {
+                    name: 'posRep',
+                    type: 'uint253',
+                    updateBy: 'sum',
+                },
+                {
+                    name: 'posRep1',
+                    type: 'uint253',
+                    updateBy: 'sum',
+                },
+                {
+                    name: 'posRep2',
+                    type: 'uint253',
+                    updateBy: 'sum',
+                },
+                {
+                    name: 'posRep3',
+                    type: 'uint253',
+                    updateBy: 'sum',
+                },
+                {
+                    name: 'posRep4',
+                    type: 'uint253',
+                    updateBy: 'sum',
+                },
+                {
+                    name: 'posRep5',
+                    type: 'uint253',
+                    updateBy: 'sum',
+                },
+                {
+                    name: 'posRep6',
+                    type: 'uint253',
+                    updateBy: 'sum',
+                },
+            ]
+            expect(() => new DataSchema(invalidSchema)).to.throw(
+                'Invalid schema, field "posRep4" exceeds available storage'
+            )
+        }
+        {
+            const invalidSchema = [
+                {
+                    name: 'graffiti',
+                    type: 'uint205',
+                    updateBy: 'replace',
+                },
+                {
+                    name: 'graffiti1',
+                    type: 'uint205',
+                    updateBy: 'replace',
+                },
+                {
+                    name: 'graffiti2',
+                    type: 'uint205',
+                    updateBy: 'replace',
+                },
+            ]
+            expect(() => new DataSchema(invalidSchema)).to.throw(
+                'Invalid schema, field "graffiti2" exceeds available storage'
+            )
+        }
+    })
+
+    it('fail to use invalid `updateBy` strategy', () => {
+        const invalidSchema = [
+            {
+                name: 'posRep',
+                type: 'uint253',
+                updateBy: 'invalidUpdateStrategy',
+            },
+            {
+                name: 'graffiti',
+                type: 'uint205',
+                updateBy: 'replace',
+            },
+        ]
+        expect(() => new DataSchema(invalidSchema)).to.throw(
+            'Invalid updateBy strategy for field posRep: "invalidUpdateStrategy"'
+        )
+    })
+
+    it('fail to use invalid uint size', () => {
+        {
+            const invalidSchema = [
+                {
+                    name: 'posRep',
+                    type: 'uint254',
+                    updateBy: 'sum',
+                },
+                {
+                    name: 'graffiti',
+                    type: 'uint205',
+                    updateBy: 'replace',
+                },
+            ]
+            expect(() => new DataSchema(invalidSchema)).to.throw(
+                'Invalid uint size for field posRep: 254'
+            )
+        }
+        {
+            const invalidSchema = [
+                {
+                    name: 'posRep',
+                    type: 'uint253',
+                    updateBy: 'sum',
+                },
+                {
+                    name: 'graffiti',
+                    type: 'uint206',
+                    updateBy: 'replace',
+                },
+            ]
+            expect(() => new DataSchema(invalidSchema)).to.throw(
+                'Invalid uint size for field graffiti: 206'
+            )
+        }
+    })
+})
+
+describe('Check `update`', function () {
+    it('successfully update sum fields', () => {
+        let x = Array(schema.length).fill(0n)
+        const d = new DataSchema(schema)
+        Array(10)
+            .fill(0)
+            .forEach(() => {
+                d.parseSchema(schema).forEach((field, i) => {
+                    if (field.updateBy !== 'sum') return
+                    const rand = smallRandom(field.bits)
+                    d.update(field.name, BigInt(rand))
+                    x[i] += BigInt(rand)
+                    expect(BigInt(d.parseData(d.data)[field.name])).to.equal(
+                        x[i]
+                    )
+                })
+            })
+    })
+    it('successfully update replacement fields', () => {
+        const d = new DataSchema(schema)
+        Array(10)
+            .fill(0)
+            .forEach(() => {
+                d.parseSchema(schema).forEach((field) => {
+                    if (field.updateBy !== 'replace') return
+                    const rand = smallRandom(field.bits)
+                    d.update(field.name, BigInt(rand))
+                    expect(d.parseData(d.data)[field.name]).to.equal(rand)
+                })
+            })
+    })
+    it('fails to use exceedingly large number', () => {
+        {
+            const d = new DataSchema(schema)
+            expect(() => d.update('posRep', 1n << 254n)).to.throw(
+                'posRep value exceeds allocated storage'
+            )
+        }
+        {
+            const d = new DataSchema(schema)
+            expect(() => d.update('graffiti', 1n << 206n)).to.throw(
+                'graffiti value exceeds allocated storage'
+            )
+        }
+    })
+})
+
+describe('Parse encoded data', () => {
+    it('Parse data', () => {
+        const d = new DataSchema(schema)
+        let x = Array(schema.length).fill(0n)
+        Array(10)
+            .fill(0)
+            .forEach(() => {
+                d.parseSchema(schema).forEach((field, i) => {
+                    const rand = smallRandom(field.bits)
+                    d.update(field.name, BigInt(rand))
+                    x[i] =
+                        field.updateBy === 'sum'
+                            ? x[i] + BigInt(rand)
+                            : BigInt(rand)
+                })
+            })
+
+        const data = d.parseData(d.data)
+        Object.keys(data).forEach((field, i) => {
+            expect(data[field]).to.equal(x[i])
+        })
+    })
+})
