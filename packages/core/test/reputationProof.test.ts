0271ddaf82d973ecb60c899818fedf935c9e0263
YURIKO NISHIJIMA <yuriko.nishijima@gmail.com>
2023-05-05 12:28:35 -0400

    add mirep and maxrep test

--- packages/core/test/reputationProof.test.ts
+++ packages/core/test/reputationProof.test.ts
@@ -136,6 +136,153 @@ describe('Reputation proof', function () {
     })
 
     // TODO: should prove minRep, maxRep, graffiti
+    it('should prove minRep', async () => {
+        const accounts = await ethers.getSigners()
+        const attester = accounts[1]
+        const attesterId = BigInt(attester.address)
+        const id = new Identity()
+        const userState = await genUserState(
+            ethers.provider,
+            unirepContract.address,
+            id,
+            attesterId
+        )
+        const epoch = await userState.sync.loadCurrentEpoch()
+        {
+            const { publicSignals, proof } = await userState.genUserSignUpProof(
+                { epoch }
+            )
+            await unirepContract
+                .connect(attester)
+                .userSignUp(publicSignals, proof)
+                .then((t) => t.wait())
+        }
+        await userState.waitForSync()
+
+        const minRep = 1
+        const epochKey = genEpochKey(id.secret, attester.address, epoch, 0)
+        const field = userState.sync.settings.sumFieldCount
+
+        await unirepContract
+            .connect(attester)
+            .attest(epochKey, epoch, field, minRep)
+            .then((t) => t.wait())
+
+        // setting posRep to 5
+        await unirepContract
+            .connect(attester)
+            .attest(epochKey, epoch, 0, 5)
+            .then((t) => t.wait())
+
+        // setting negRep to 3
+        await unirepContract
+            .connect(attester)
+            .attest(epochKey, epoch, 1, 3)
+            .then((t) => t.wait())
+
+        await ethers.provider.send('evm_increaseTime', [EPOCH_LENGTH])
+        await ethers.provider.send('evm_mine', [])
+
+        const toEpoch = await unirepContract.attesterCurrentEpoch(
+            attester.address
+        )
+        {
+            await userState.waitForSync()
+            const { publicSignals, proof } =
+                await userState.genUserStateTransitionProof({
+                    toEpoch,
+                })
+            await unirepContract
+                .connect(accounts[4])
+                .userStateTransition(publicSignals, proof)
+                .then((t) => t.wait())
+        }
+
+        await userState.waitForSync()
+        const proof = await userState.genProveReputationProof({
+            minRep,
+        })
+
+        const valid = await proof.verify()
+        expect(valid).to.be.true
+        expect(proof.minRep).to.equal(minRep.toString())
+        expect(proof.proveMinRep).to.equal('1')
+        userState.sync.stop()
+    })
+
+    it('should prove maxRep', async () => {
+        const accounts = await ethers.getSigners()
+        const attester = accounts[1]
+        const attesterId = BigInt(attester.address)
+        const id = new Identity()
+        const userState = await genUserState(
+            ethers.provider,
+            unirepContract.address,
+            id,
+            attesterId
+        )
+        const epoch = await userState.sync.loadCurrentEpoch()
+        {
+            const { publicSignals, proof } = await userState.genUserSignUpProof(
+                { epoch }
+            )
+            await unirepContract
+                .connect(attester)
+                .userSignUp(publicSignals, proof)
+                .then((t) => t.wait())
+        }
+        await userState.waitForSync()
+
+        const maxRep = 2
+        const epochKey = genEpochKey(id.secret, attester.address, epoch, 0)
+        const field = userState.sync.settings.sumFieldCount
+
+        await unirepContract
+            .connect(attester)
+            .attest(epochKey, epoch, field, maxRep)
+            .then((t) => t.wait())
+
+        // setting posRep to 5
+        await unirepContract
+            .connect(attester)
+            .attest(epochKey, epoch, 0, 5)
+            .then((t) => t.wait())
+
+        // setting negRep to 10
+        await unirepContract
+            .connect(attester)
+            .attest(epochKey, epoch, 1, 10)
+            .then((t) => t.wait())
+
+        await ethers.provider.send('evm_increaseTime', [EPOCH_LENGTH])
+        await ethers.provider.send('evm_mine', [])
+
+        const toEpoch = await unirepContract.attesterCurrentEpoch(
+            attester.address
+        )
+        {
+            await userState.waitForSync()
+            const { publicSignals, proof } =
+                await userState.genUserStateTransitionProof({
+                    toEpoch,
+                })
+            await unirepContract
+                .connect(accounts[4])
+                .userStateTransition(publicSignals, proof)
+                .then((t) => t.wait())
+        }
+
+        await userState.waitForSync()
+        const proof = await userState.genProveReputationProof({
+            maxRep,
+        })
+
+        const valid = await proof.verify()
+        expect(valid).to.be.true
+        expect(proof.maxRep).to.equal(maxRep.toString())
+        expect(proof.proveMaxRep).to.equal('1')
+        userState.sync.stop()
+    })
 
     it('should prove graffiti', async () => {
         const accounts = await ethers.getSigners()
