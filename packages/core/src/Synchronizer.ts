281512a8d07c0a706e77e9d61e4170a5f90a1399
Chance <jchancehud@gmail.com>
2023-05-16 02:09:03 -0500

    feat(core): register event handlers in synchronizer after constructor

--- packages/core/src/Synchronizer.ts
+++ packages/core/src/Synchronizer.ts
@@ -96,6 +96,13 @@ export class Synchronizer extends EventEmitter {
             sumFieldCount: 0,
             replNonceBits: 0,
         }
+        new Promise((r) => setTimeout(r, 0))
+            .then(() => this.buildEventHandlers())
+            .then(() => this.setup())
+            .then(() => (this.setupComplete = true))
+    }
+
+    private buildEventHandlers() {
         const allEventNames = {} as any
 
         this._eventHandlers = Object.keys(this.contracts).reduce(
@@ -168,7 +175,6 @@ export class Synchronizer extends EventEmitter {
             },
             {}
         )
-        this.setup().then(() => (this.setupComplete = true))
     }
 
     get attesterId() {
