630c28fa99fc13f15ed9a5a5f32b490db33cb7ce
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2023-07-07 17:18:41 +0800

    chore(utils): update functions

--- packages/utils/src/stringify.ts
+++ packages/utils/src/stringify.ts
@@ -1,5 +1,5 @@
 // Little endian Uint8Array to BigInt
-function fromLE(arr) {
+function fromLE(arr: any): BigInt {
     let val = BigInt(0)
     let base = BigInt(1)
     for (const byte of arr) {
@@ -9,7 +9,7 @@ function fromLE(arr) {
     return val
 }
 
-export function stringifyBigInts(o) {
+export function stringifyBigInts(o: any): any {
     if (typeof o == 'bigint' || o.eq !== undefined) {
         return o.toString(10)
     } else if (o instanceof Uint8Array) {
@@ -28,7 +28,7 @@ export function stringifyBigInts(o) {
     }
 }
 
-export function unstringifyBigInts(o) {
+export function unstringifyBigInts(o: any): any {
     if (typeof o == 'string' && /^[0-9]+$/.test(o)) {
         return BigInt(o)
     } else if (typeof o == 'string' && /^0x[0-9a-fA-F]+$/.test(o)) {
