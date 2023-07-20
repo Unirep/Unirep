3f60de86cd01bf96c9d27b58c974f585f4d3af76
Chiali <info@chialitsai.com>
2023-07-20 17:14:54 -0400

    minor ui updates and adding audit (#514)
    * attempt to add content, minor ui updates
    
    * adding audit report and minor theme update
    
    * fix: lint, typos
    
    ---------
    
    Co-authored-by: Ya-wen, Jeng <vivi432@yahoo.com.tw>

--- packages/docs/src/pages/index.js
+++ packages/docs/src/pages/index.js
@@ -4,6 +4,7 @@ import Link from '@docusaurus/Link'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import Layout from '@theme/Layout'
 import HomepageFeatures from '@site/src/components/HomepageFeatures'
+import { Hero } from '@site/src/components/Hero'
 
 import styles from './index.module.css'
 
