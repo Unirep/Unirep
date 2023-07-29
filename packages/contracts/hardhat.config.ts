9202e0fa704ddde02f6ab158ae70fe25f1466a4b
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2023-07-29 14:20:32 +0200

    chore: update core, contract packages

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -21,7 +21,7 @@ const config: HardhatUserConfig = {
     solidity: {
         compilers: [
             {
-                version: '0.8.17',
+                version: '0.8.21',
                 settings: {
                     optimizer: { enabled: true, runs: 2 ** 32 - 1 },
                 },
